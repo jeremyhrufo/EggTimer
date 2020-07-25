0c2d2d84a8ae77e96fc8bf46c4b3ba2fff650469
Jeremy Rufo <jeremyhrufo@gmail.com>
2020-07-25 11:46:38 -0400

    Re-cloned this project - lost my original changes when my mac crashed and I had to exchange the computer
    Brought down the completed project for reference and updated it to Xcode Beta 12 and iOS 14

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,15 +2,15 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Jeremy Rufo
+//  Copyright © 2020 Jeremy H Rufo
 //
 
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController
+{
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
@@ -19,8 +19,8 @@ class ViewController: UIViewController {
     var totalTime = 0
     var secondsPassed = 0
     
-    @IBAction func hardnessSelected(_ sender: UIButton) {
-        
+    @IBAction func hardnessSelected(_ sender: UIButton)
+    {
         timer.invalidate()
         let hardness = sender.currentTitle!
         totalTime = eggTimes[hardness]!
@@ -32,12 +32,16 @@ class ViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
     }
     
-    @objc func updateTimer() {
-        if secondsPassed < totalTime {
+    @objc func updateTimer()
+    {
+        if secondsPassed < totalTime
+        {
             secondsPassed += 1
             progressBar.progress = Float(secondsPassed) / Float(totalTime)
             print(Float(secondsPassed) / Float(totalTime))
-        } else {
+        }
+        else
+        {
             timer.invalidate()
             titleLabel.text = "DONE!"
             
@@ -46,5 +50,4 @@ class ViewController: UIViewController {
             player.play()
         }
     }
-    
 }
